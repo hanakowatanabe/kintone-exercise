f34b51cd9ac6735471d5e0b7b2e3d2fafb8e6f17
flexfrank@gmail.com <flexfrank@gmail.com>
2014-04-10 09:34:14 +0900

    obey JSLint

--- 10/color.js
+++ 10/color.js
@@ -1,22 +1,25 @@
 /*
  * Licensed under the MIT License
  */
+/*jslint nomen: true, devel: true, browser: true, vars: true, plusplus: true*/
+/*global 'kintone': false */
 (function () {
   
     "use strict";
     // レコード一覧の表示時にフィールドの背景色を変更する
     kintone.events.on('app.record.index.show', function (event) {
-        var fieldColor = '#e5f0ff';        // ログインユーザのフィールド色
+            // ログインユーザのフィールド色
+        var fieldColor = '#e5f0ff',
+            // 一覧の要素を取得
+            elCustomer = kintone.app.getFieldElements('Customer'),
+            elStatus = kintone.app.getFieldElements('Status'),
+            elPerson = kintone.app.getFieldElements('Person'),
+            elQType = kintone.app.getFieldElements('QType'),
+            elDetail = kintone.app.getFieldElements('Detail'),
+            elLimitDay = kintone.app.getFieldElements('LimitDay'),
+            i;
   
-        // 一覧の要素を取得
-        var elCustomer = kintone.app.getFieldElements('Customer');
-        var elStatus = kintone.app.getFieldElements('Status');
-        var elPerson = kintone.app.getFieldElements('Person');
-        var elQType = kintone.app.getFieldElements('QType');
-        var elDetail = kintone.app.getFieldElements('Detail');
-        var elLimitDay = kintone.app.getFieldElements('LimitDay');
-  
-        for (var i = 0; i < event.records.length; i++) {
+        for (i = 0; i < event.records.length; i++) {
             elCustomer[i].style.backgroundColor = fieldColor;
             elStatus[i].style.backgroundColor = fieldColor;
             elPerson[i].style.backgroundColor = fieldColor;
@@ -25,4 +28,4 @@
             elLimitDay[i].style.backgroundColor = fieldColor;
         }
     };
-})();
+}());
