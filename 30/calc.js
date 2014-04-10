fe462f9d8530f5b866fa28e472601d0ddc084df2
flexfrank@gmail.com <flexfrank@gmail.com>
2014-04-10 16:17:47 +0900

    add task 30

---
+++ 30/calc.js
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) 2014 Cybozu
+ * Licensed under the MIT License
+ */
+/*jslint nomen: true, devel: true, browser: true, vars: true, plusplus: true*/
+/*global 'kintone': false */
+(function () {
+    "use strict";
+    kintone.events.on('app.record.index.show', function (event) {
+        var headerSpace = kintone.app.getHeaderSpaceElement(),
+            textNode = null,
+            sum = 0;
+
+        //既に入っている要素を削除
+        headerSpace.innerHTML = '';
+        
+        sum = parseInt(event.records[1]['数値_1'].value, 10);
+        textNode = document.createTextNode('合計金額: ' + sum);
+        headerSpace.appendChild(textNode);
+    });
+}());
