102dba78b84333122a8bc7b200722e1385a78754
flexfrank@gmail.com <flexfrank@gmail.com>
2014-04-10 11:03:34 +0900

    add task 20

--- 10/color.js
+++ 10/color.js
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2014 Cybozu
  * Licensed under the MIT License
  */
 /*jslint nomen: true, devel: true, browser: true, vars: true, plusplus: true*/
---
+++ 20/user_fill.js
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2014 Cybozu
+ * Licensed under the MIT License
+ */
+/*jslint nomen: true, devel: true, browser: true, vars: true, plusplus: true*/
+/*global 'kintone': false */
+(function () {
+  
+    "use strict";
+    kintone.events.on('app.record.create.show', function (event) {
+        event.record.Person.value = [{'code': test, 'name': テスト}];
+        
+        return event;
+    });
+}());
