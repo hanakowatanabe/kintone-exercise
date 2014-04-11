b5c7b14622f83458598c34911c7160344c05bd01
flexfrank@gmail.com <flexfrank@gmail.com>
2014-04-11 15:17:18 +0900

    add validation task

---
+++ 20/user_limit.js
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2014 Cybozu
+ * Licensed under the MIT License
+ */
+/*jslint nomen: true, devel: true, browser: true, vars: true, plusplus: true*/
+/*global 'kintone': false */
+(function () {
+  
+    "use strict";
+    
+    var events = [
+        'app.record.edit.submit',
+        'app.record.create.submit',
+        'app.record.index.edit.submit'
+    ];
+    kintone.events.on(events, function (event) {
+        var size = event.record.Person.value.length;
+        //対応担当者に6人より多く登録しようとすると
+        //'対応担当者は5人までです。' というエラーを表示
+        return event;
+    });
+}());
