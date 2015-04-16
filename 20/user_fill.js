77e5f0b4a35cc65641dd1336dc289ed9c408556d
unknown <a000551@pc-jp-000551-1.cb.local>
2015-04-16 15:39:26 +0900

    問題２２

--- 20/user_fill.js
+++ 20/user_fill.js
@@ -8,7 +8,8 @@
   
     "use strict";
     kintone.events.on('app.record.create.show', function (event) {
-        event.record.Person.value = [{'code': 'yuichi-sugiyama'}];
+        var user = kintone.getLoginUser();         
+        event.record.Person.value = [{'code': user.code }];
         
         return event;
     });
