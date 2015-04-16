608516e9f83ad018626d93476aa98fa0569f7f04
unknown <a000551@pc-jp-000551-1.cb.local>
2015-04-16 14:27:47 +0900

    問題１２

--- 10/color.js
+++ 10/color.js
@@ -10,17 +10,17 @@
     // レコード一覧の表示時にフィールドの背景色を変更する
     kintone.events.on('app.record.index.show', function (event) {
             // ログインユーザのフィールド色
-            //var fieldColors = ["#87cefa", "#adff2f", "#ffd700", "#ff6347", "#d3d3d3","#4b0082"];
-            var fieldColor = "#e5f0ff",
-            //fieldColors[Math.floor(Math.random() * 6)];
+            var fieldColors = ["#87cefa", "#adff2f", "#ffd700", "#ff6347", "#d3d3d3", "#4b0082"],
+            //var fieldColor = "#e5f0ff",
+            i = Math.floor(Math.random() * fieldColors.length),
+            fieldColor = fieldColors[i],
             // 一覧の要素を取得
             elCustomer = kintone.app.getFieldElements('Customer'),
             elStatus = kintone.app.getFieldElements('Status'),
             elPerson = kintone.app.getFieldElements('Person'),
             elQType = kintone.app.getFieldElements('QType'),
             elDetail = kintone.app.getFieldElements('Detail'),
-            elLimitDay = kintone.app.getFieldElements('LimitDay'),
-            i;
+            elLimitDay = kintone.app.getFieldElements('LimitDay');
   
         for (i = 0; i < event.records.length; i++) {
             if (i % 2 == 1) {
