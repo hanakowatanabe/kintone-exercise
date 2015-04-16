03c2fddafbf173882581071928c02b266336fc2b
unknown <a000551@pc-jp-000551-1.cb.local>
2015-04-16 13:20:51 +0900

    問題１０

--- 10/color.js
+++ 10/color.js
@@ -7,21 +7,20 @@
 (function () {
   
     "use strict";
-    var fieldColor = '#e5f0ff';
     // レコード一覧の表示時にフィールドの背景色を変更する
     kintone.events.on('app.record.index.show', function (event) {
             // ログインユーザのフィールド色
-            
+        var fieldColor = '#e5f0ff',
             // 一覧の要素を取得
-            var elCustomer = kintone.app.getFieldElements('Customer');
-            var elQType = kintone.app.getFieldElements('QType');
-            var elStatus = kintone.app.getFieldElements('Status');
-            var elLimitDay = kintone.app.getFieldElements('LimitDay');
-            var elPerson = kintone.app.getFieldElements('Person');
-            var elDetail = kintone.app.getFieldElements('Detail');
-            //i;
+            elCustomer = kintone.app.getFieldElements('Customer'),
+            elStatus = kintone.app.getFieldElements('Status'),
+            elPerson = kintone.app.getFieldElements('Person'),
+            elQType = kintone.app.getFieldElements('QType'),
+            elDetail = kintone.app.getFieldElements('Detail'),
+            elLimitDay = kintone.app.getFieldElements('LimitDay'),
+            i;
   
-        for (var i = 0; i < event.records.length; i++) {
+        for (i = 0; i < event.records.length; i++) {
             elCustomer[i].style.backgroundColor = fieldColor;
             elStatus[i].style.backgroundColor = fieldColor;
             elPerson[i].style.backgroundColor = fieldColor;
@@ -30,5 +29,4 @@
             elLimitDay[i].style.backgroundColor = fieldColor;
         }
     });
-                     
 }());
